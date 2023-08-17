2598e73e0f0dbe0ae76cb0c71a9266a153a331e1
Heja <arslanheja202@gmail.com>
2023-08-17 12:07:29 +0300

    Ready version 0.1

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # Math-Interpreter
-This is a interpreter of math expressions
+
+This is a interpreter of math expressions.
+
+Hello, I can say this: "This is my first and successful project in Programming language theory".
+I made an interpreter to interpret math expressions and share it with you.
+
+**Note**: Please do not worry that they are all in one file.
+They will all be categorized.
+
+**How to work**:
+
+**Lexer**: When we write anythings interpreters can not read that as directly and can't parse, we separate text (input) as tokens. If token is correct, it publish with its type and value.
+
+**Parser**:  
+ Here is may be confuse.
+
+**(Parser) Method of eat**:
+If taken token is expected token, eats that and next other token.
+Thanks to method, we know our way.
+
+**(Parser) Method of parseFactor**:
+Convert potential tokens to integer from string text and thanks to which we can parse they.
+
+**(Parser) Method of parseExpression**:
+We processing multiply and divide in here.
+When we processing these, we offer integer from parseFactor.
+We take first integer with result variable like: (2) _ 2, and we check if token multiply or divide: 2 (_) 2 if like that, remember we assign result first integer, opposed potential multiply op: (2) \* 2, now we assign to variable of result last integer result by multiplying result.
+
+**(Parser) parseTerm method**:
+The logic of the function is the same as parseExpression.
+So why don't we use parseFactor?
+
+Because we get the result of all multiplication and division operations, and with eat func we pass all the operations processed and store the result in the result variable. Thus, there are no plus and minus operations in parseExpression and the parameter related to parseTerm operations comes to the parseTerm method with the result variable.
---
+++ main.go
@@ -0,0 +1,274 @@
+package main
+
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+/* Types start */
+// _replace shortcut of strings.ReplaceAll
+var _replace func(s string, old string, new string) string = strings.ReplaceAll
+
+// For keep Lexer informations
+type TokenType int
+
+// For keep lexer information
+type Lexer struct {
+	text        string
+	pos         int
+	currentChar string
+}
+
+// For keep token information
+type Token struct {
+	Type  TokenType
+	Value string
+}
+
+// Structure
+type Parser struct {
+	lexer        *Lexer
+	currentToken Token
+}
+
+// If we add a token here and do not use it, the loop will not stop
+const (
+	// Type for tokens like +, *
+	TokenInteger TokenType = iota
+	TokenPlus
+	TokenMinus
+	TokenMultiply
+	TokenDivide
+	TokenLParen
+	TokenRParen
+	TokenEOF
+)
+
+/* Types End */
+
+/* Lexer Start */
+
+// Return a lexer from memory
+func NewLexer(text string) *Lexer {
+	return &Lexer{
+		text: text,
+		pos:  0,
+	}
+}
+
+// Check token is integer?
+func isDigit(ch byte) bool {
+	return '0' <= ch && ch <= '9'
+}
+
+// Check token is operator of math?
+func isOperator(ch byte) bool {
+	return '*' == ch || '+' == ch || '-' == ch || '/' == ch
+}
+
+// Take other char if exists
+func (l *Lexer) advanceChar() {
+	if l.pos < len(l.text) {
+		l.currentChar = string(l.text[l.pos])
+		l.pos++
+	} else {
+		l.currentChar = ""
+	}
+}
+
+// Skip whitespace
+func (l *Lexer) skipWhiteSpace() {
+	for l.currentChar == " " {
+		l.advanceChar()
+	}
+}
+
+// Move to the next character after spaces
+func (l *Lexer) jumpSpaceNextChar() {
+	l.advanceChar()
+	l.skipWhiteSpace()
+}
+
+// Lexical analysis
+func (l *Lexer) lexer() Token {
+	for l.pos < len(l.text) {
+		currentChar := l.text[l.pos]
+
+		if currentChar == ' ' {
+			l.skipWhiteSpace()
+			continue
+		}
+
+		if isDigit(currentChar) {
+			num := ""
+			for l.pos < len(l.text) && isDigit(l.text[l.pos]) {
+				num += string(l.text[l.pos])
+				l.pos++
+			}
+			return Token{Type: TokenInteger, Value: num}
+		}
+
+		switch currentChar {
+		case '+':
+			l.advanceChar()
+			return Token{Type: TokenPlus, Value: "+"}
+		case '-':
+			l.advanceChar()
+			return Token{Type: TokenMinus, Value: "-"}
+		case '*':
+			l.advanceChar()
+			return Token{Type: TokenMultiply, Value: "*"}
+		case '/':
+			l.advanceChar()
+			return Token{Type: TokenDivide, Value: "/"}
+		case '(':
+			l.advanceChar()
+			return Token{Type: TokenLParen, Value: "("}
+		case ')':
+			l.advanceChar()
+			return Token{Type: TokenRParen, Value: ")"}
+		default:
+			return Token{Type: TokenEOF, Value: ""}
+		}
+	}
+
+	return Token{Type: TokenEOF, Value: ""}
+}
+
+/* Lexer End */
+
+/* Parser Start */
+
+func NewParser(lexer *Lexer) *Parser {
+	parser := &Parser{lexer: lexer}
+
+	// Start with a token
+	parser.currentToken = parser.lexer.lexer()
+	return parser
+}
+
+// Check token if it expected type then assign as current token
+func (p *Parser) eat(tokenType TokenType) {
+	if p.currentToken.Type == tokenType {
+		// Token is correct then assign current token
+		p.currentToken = p.lexer.lexer()
+	} else {
+		// If token type equal to TokenEof throw panic
+		if p.currentToken.Type == TokenEOF {
+			panic(fmt.Sprintf("Expected %v but got %v", tokenType, p.currentToken))
+		}
+	}
+}
+
+func (p *Parser) parseFactor() int {
+	token := p.currentToken
+	p.eat(TokenInteger)
+
+	number, _ := strconv.Atoi(token.Value)
+	return number
+}
+
+func (p *Parser) parseExpression() int {
+	result := p.parseFactor()
+
+	// Only does work for '*' and '/'
+	for p.currentToken.Type == TokenMultiply || p.currentToken.Type == TokenDivide {
+		token := p.currentToken
+
+		if token.Type == TokenMultiply {
+			// we wait expected the data
+			p.eat(TokenMultiply)
+			result *= p.parseFactor()
+		} else if token.Type == TokenDivide {
+			// we wait expected the data
+			p.eat(TokenDivide)
+			result /= p.parseFactor()
+		}
+	}
+
+	// Return result of process '*' and '/'
+	return result
+}
+
+func (p *Parser) parseTerm() int {
+	result := p.parseExpression()
+	// Only does work for '+' and '-'
+	for p.currentToken.Type == TokenPlus || p.currentToken.Type == TokenMinus {
+		token := p.currentToken
+
+		if token.Type == TokenPlus {
+			// we wait expected the data
+			p.eat(TokenPlus)
+			result += p.parseExpression()
+		} else if token.Type == TokenMinus {
+			// we wait expected the data
+			p.eat(TokenMinus)
+			result -= p.parseExpression()
+		}
+	}
+
+	// Return result of process '+' and '-'
+	return result
+}
+
+/* Parser end */
+
+/*  Start Error */
+
+func minLength(text string) error {
+	if len(text) < 2 {
+		return errors.New("You must start with three length text like: 2 + 2")
+	}
+	return nil
+}
+
+func checkOP(sChar byte, eChar byte) error {
+	if (sChar == '*' || sChar == '+' || sChar == '-' || sChar == '/') ||
+		(eChar == '*' || eChar == '+' || eChar == '-' || eChar == '/') {
+
+		return errors.New("Token can not be operator, at last and start of input")
+	}
+	return nil
+}
+
+/* End Error */
+
+func main() {
+	var input string
+	for input != "exit" {
+		fmt.Println("Enter 'exit' for leave the terminal of interpreter:")
+		fmt.Print("> ")
+
+		scanner := bufio.NewScanner(os.Stdin)
+		if scanner.Scan() {
+			input = scanner.Text()
+		}
+
+		if input == "exit" {
+			break
+		}
+
+		text := strings.ReplaceAll(input, " ", "")
+
+		if err := minLength(text); err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		startCh, lastCh := text[0], text[len(text)-1]
+		if err := checkOP(startCh, lastCh); err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		lexer := NewLexer(text)
+		parser := NewParser(lexer)
+		result := parser.parseTerm()
+
+		fmt.Printf("%d\n", result)
+	}
+}
